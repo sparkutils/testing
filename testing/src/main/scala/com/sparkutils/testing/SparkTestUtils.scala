4648ea364906c6ef045874979446a4c1e3043d97
Chris Twiner <chris.twiner@gmail.com>
2025-10-26 15:11:53 +0100

    bring back seperate aqe plan handling

--- testing/src/main/scala/com/sparkutils/testing/SparkTestUtils.scala
+++ testing/src/main/scala/com/sparkutils/testing/SparkTestUtils.scala
@@ -2,9 +2,6 @@ package com.sparkutils.testing
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.catalyst.expressions.Expression
-import org.apache.spark.sql.execution.SparkPlan
-import org.apache.spark.sql.execution.adaptive.AdaptiveSparkPlanExec
-
 import java.util.concurrent.atomic.AtomicReference
 
 object SparkTestUtils {
@@ -22,16 +19,6 @@ object SparkTestUtils {
 
   def resolveBuiltinOrTempFunction(sparkSession: SparkSession)(name: String, exps: Seq[Expression]): Option[Expression] =
     ClassicSparkTestUtils.resolveBuiltinOrTempFunction(sparkSession)(name, exps)
-
-  def getCorrectPlan(sparkPlan: SparkPlan): SparkPlan =
-    if (sparkPlan.children.isEmpty)
-    // assume it's AQE
-      sparkPlan match {
-        case aq: AdaptiveSparkPlanExec => aq.initialPlan
-        case _ => sparkPlan
-      }
-    else
-      sparkPlan
 
   def enumToScala[A](enum: java.util.Enumeration[A]) = {
     import scala.collection.JavaConverters._
