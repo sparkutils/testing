9c81de056baefb66a7204433929aee83aff15b28
Chris Twiner <chris.twiner@gmail.com>
2025-11-01 21:52:47 +0100

    make testrunner usable

---
+++ testing/src/main/scala/com/sparkutils/testing/TestRunner.scala
@@ -0,0 +1,140 @@
+package com.sparkutils.testing
+
+import com.google.common.reflect.ClassPath
+import org.scalatest.Suite
+
+import java.io.IOException
+import java.util
+
+/**
+ * Extend this trait as an object to create a test runner
+ */
+trait TestRunner {
+
+  /**
+   * Search this package name to find tests
+   */
+  val packageName: String
+
+  /**
+   * ClassLoader used to find tests by default uses this classloader
+   */
+  val classLoader: ClassLoader = this.getClass.getClassLoader
+
+  /**
+   * Project name to use for stdout logging
+   */
+  val projectName: String
+
+  /**
+   * Provides -oWDFT by default
+   */
+  val scalaTestArgs: Seq[String] = Seq("-oWDFT")
+
+  def numberOfBatches(numberOfClasses: Int) = numberOfClasses / 2 / 10
+  val argsPerBatch = 20
+
+  @throws[IOException]
+  def testViaClassLoader(batchStartingNumber: Int): Unit = {
+    testViaClassLoader(Array.empty[String], batchStartingNumber)
+  }
+
+  @throws[IOException]
+  def testViaClassLoader(args: Array[String]): Unit = {
+    testViaClassLoader(args, 0)
+  }
+
+  @throws[IOException]
+  def testViaClassLoader(args: Array[String], batchStartingNumber: Int): Unit = {
+    val oargs = new util.ArrayList[String]
+    scalaTestArgs.foreach(oargs + _)
+
+    for (i <- 0 until args.length) {
+      oargs.add(args(i))
+    }
+    val classargs = new util.ArrayList[String]
+    val classPath = ClassPath.from(classLoader)
+    val framelessInfo = classPath.getTopLevelClassesRecursive(packageName)
+    val itr = framelessInfo.iterator()
+    while(itr.hasNext) {
+      try {
+        val clazz = itr.next().load()
+        val suite = clazz.newInstance.asInstanceOf[Suite]
+        // it's a suite
+        classargs.add("-s")
+        classargs.add(clazz.getName)
+      } catch {
+        case t: Throwable =>
+        // ignore
+      }
+    }
+    val numberOfBatches = numberOfBatches(classargs.size)
+    
+    val classargItr = classargs.iterator
+    var j = 0
+    while (j < (batchStartingNumber * argsPerBatch) && classargItr.hasNext) {
+      classargItr.next
+
+      j += 1
+    }
+    for (i <- batchStartingNumber until numberOfBatches) {
+      System.out.println(s"$projectName - starting test batch $i")
+      val joined = new Array[String](oargs.size + argsPerBatch)
+      oargs.toArray[String](joined)
+      var j = 0
+      while (j < argsPerBatch && classargItr.hasNext) {
+        joined(oargs.size + j) = classargItr.next
+
+        j += 1
+      }
+      org.scalatest.tools.Runner.run(joined)
+      System.out.println(s"projectName - gc'ing after finishing test batch $i")
+      System.gc()
+      System.gc()
+    }
+    System.out.println(s"all $projectName test batches completed")
+  }
+
+  /**
+   * Run against any test found in the current classloader
+   *
+   * @param args
+   */
+  def scalaTestRunner(args: Array[String]): Unit = {
+    org.scalatest.tools.Runner.run(args)
+  }
+
+  @throws[IOException]
+  def test(batchStartingNumber: Int): Unit = {
+    test(Array.empty[String], batchStartingNumber)
+  }
+
+  @throws[IOException]
+  def test(args: Array[String]): Unit = {
+    test(args, 0)
+  }
+
+  @throws[IOException]
+  def test(args: Array[String], batchStartingNumber: Int): Unit = {
+    testViaClassLoader(args, batchStartingNumber)
+  }
+
+  @throws[IOException]
+  def runTestName(testName: String): Unit = {
+    val oargs = new util.ArrayList[String]
+    oargs.add("-oWDFT")
+    oargs.add("-s")
+    oargs.add(testName)
+    val joined = new Array[String](oargs.size)
+    oargs.toArray[String](joined)
+    org.scalatest.tools.Runner.run(joined)
+    System.out.println(s"$projectName - $testName finished properly")
+  }
+
+  @throws[IOException]
+  def test(): Unit = {
+    test(Array.empty[String])
+  }
+
+
+}
