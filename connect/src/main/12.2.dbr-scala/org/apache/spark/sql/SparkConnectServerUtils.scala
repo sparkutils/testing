e8e2628773566fec1d46de17b6a1b1f2aa0e7f63
Chris Twiner <chris.twiner@gmail.com>
2026-05-20 18:46:26 +0200

    remove the 12.2 from build ~cut-release

--- connect/src/main/12.2.dbr-scala/org/apache/spark/sql/SparkConnectServerUtils.scala
+++
@@ -1,7 +0,0 @@
-package org.apache.spark.sql
-
-import com.sparkutils.testing.ConnectSession
-
-object SparkConnectServerUtils {
-  def localConnectServerForTesting(serverConfig: Map[String, String], clientConfig: Map[String, String]): Option[ConnectSession] = None
-}
